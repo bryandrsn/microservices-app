2c50f758b04300ab2babc65080fe9a11f7408acd
Bryan <bryanandersonnapitupulu77@gmail.com>
2026-04-27 01:44:42 +0700

    fix typo

--- service-user/seeders/20260421140127-user-seeders.js
+++ service-user/seeders/20260421140127-user-seeders.js
@@ -3,30 +3,30 @@ const bcrypt = require("bcrypt");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-    up: async (queryInterface, Sequelize) => {
-        await queryInterface.bulkInsert("users", [
-            {
-                name: "budi",
-                profession: "admin",
-                role: "admin",
-                email: "budi@gmail.com",
-                password: await bcrypt.hash("12345678", 10),
-                created_at: new Date(),
-                updated_at: new Date(),
-            },
-            {
-                name: "pandu",
-                profession: "student",
-                role: "student",
-                email: "pandu@gmail.com",
-                password: await bcrypt.hash("12345678", 10),
-                created_at: new Date(),
-                updated_at: new Date(),
-            },
-        ]);
-    },
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert("users", [
+      {
+        name: "bryan",
+        profession: "admin",
+        role: "admin",
+        email: "bryan@gmail.com",
+        password: await bcrypt.hash("12345678", 10),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: "jidan",
+        profession: "student",
+        role: "student",
+        email: "jidan@gmail.com",
+        password: await bcrypt.hash("12345678", 10),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ]);
+  },
 
-    down: async (queryInterface, Sequelize) => {
-        await queryInterface.bulkDelete("users", "null", {});
-    },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("users", "null", {});
+  },
 };
